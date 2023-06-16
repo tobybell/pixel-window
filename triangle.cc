08ffe665b2fb2787a7c10f1ff465ca6911967a80
Toby Bell <tbell@stanford.edu>
2023-06-16 08:49:53 -0700

    Add function for rectangle rotations

--- triangle.cc
+++ triangle.cc
@@ -2,8 +2,16 @@
 
 #include <cmath>
 #include <cstdio>
+#include <utility>
 
 namespace {
+
+using std::exchange;
+
+void check(bool condition) {
+  if (!condition)
+    abort();
+}
 
 template <class T>
 T const& max(T const& a, T const& b) {
@@ -59,6 +67,83 @@ Pixel lerp(Pixel const& a, Pixel const& b, float t) {
 
 Dir2 make_slope(float t) {
   return {cos(t), sin(t)};
+}
+
+void blit_top_rectangle(Canvas& canvas, float x0, float y0, float dx, float dy, float s0, float s1) {
+  check(dx >= 0.f);
+  check(dy >= 0.f);
+
+  auto dx0 = s0 * dx;
+  auto dy0 = s0 * dy;
+  auto dx1 = -s1 * dy;
+  auto dy1 = s1 * dx;
+
+  auto slope0 = dx / dy;
+  auto slope1 = -dy / dx;
+
+  auto y1 = y0 + dy0;
+  auto y2 = y0 + dy1;
+  auto y3 = y2 + dy0;
+  auto x1 = x0 + dx0;
+  auto x2 = x0 + dx1;
+  auto x3 = x1 + dx1;
+
+  auto i0 = tmp_to_pixel(y0);
+  auto ia = tmp_to_pixel(y1);
+  auto ib = tmp_to_pixel(y2);
+  auto i3 = tmp_to_pixel(y3);
+
+  auto i1 = min(ia, ib);
+  auto i2 = max(ia, ib);
+
+  for (auto i = i0; i < i1; ++i) {
+    auto y = i + .5f - y0;
+    auto j0 = tmp_to_pixel(x0 + y * slope1);
+    auto j1 = tmp_to_pixel(x0 + y * slope0);
+    for (auto j = j0; j < j1; ++j) {
+      canvas.data[i * canvas.stride + j] = color;
+    }
+  }
+
+  auto yref = ia < ib ? y1 : y2;
+  auto x0ref = ia < ib ? x0 + slope1 * (s0 * dy) : x2;
+  auto x1ref = ia < ib ? x1 : x0 + slope0 * (s1 * dx);
+  auto slope = ia < ib ? slope1 : slope0;
+  for (auto i = i1; i < i2; ++i) {
+    auto y = i + .5f - yref;
+    auto j0 = tmp_to_pixel(x0ref + y * slope);
+    auto j1 = tmp_to_pixel(x1ref + y * slope);
+    for (auto j = j0; j < j1; ++j) {
+      canvas.data[i * canvas.stride + j] = color;
+    }
+  }
+  for (auto i = i2; i < i3; ++i) {
+    auto y = i + .5f - y3;
+    auto j0 = tmp_to_pixel(x3 + y * slope0);
+    auto j1 = tmp_to_pixel(x3 + y * slope1);
+    for (auto j = j0; j < j1; ++j) {
+      canvas.data[i * canvas.stride + j] = color;
+    }
+    
+  }
+}
+
+void blit_rectangle(Canvas& canvas, Dir2 dir) {
+  // 3 regime
+
+  auto s0 = 30.f;
+  auto s1 = 20.f;
+
+  auto x0 = 45.f;
+  auto y0 = 45.f;
+
+  if (dir.x < 0 && dir.y < 0)
+    return blit_top_rectangle(canvas, x0 + s0 * dir.x - s1 * dir.y, y0 + s0 * dir.y + s1 * dir.x, -dir.x, -dir.y, s0, s1);
+  if (dir.y < 0)
+    return blit_top_rectangle(canvas, x0 + s0 * dir.x, y0 + s0 * dir.y, -dir.y, dir.x, s1, s0);
+  if (dir.x < 0)
+    return blit_top_rectangle(canvas, x0 - s1 * dir.y, y0 + s1 * dir.x, dir.y, -dir.x, s1, s0);
+  return blit_top_rectangle(canvas, x0, y0, dir.x, dir.y, s0, s1);
 }
 
 void blit_pie_slice(Canvas& canvas, float cx, float cy, float r, Pixel color, Dir2 dir0, Dir2 dir1) {
@@ -136,12 +221,13 @@ float t = 0.f;
 }
 
 void triangle(Canvas& canvas) {
-  t += .1f;
+  t += .01f;
   auto th1 = t;
   auto th2 = 1.3f * t + .5f;
   auto dir0 = make_slope(th1);
   auto dir1 = make_slope(th2);
 
   // blit_triangle(canvas);
-  blit_pie_slice(canvas, 45., 45., 30., color, dir0, dir1);
+  // blit_pie_slice(canvas, 45., 45., 30., color, dir0, dir1);
+  blit_rectangle(canvas, dir0);
 }
